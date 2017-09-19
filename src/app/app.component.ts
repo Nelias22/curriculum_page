f49ec98d5b1dc0da06165b46e12b3e0de3b3997b
Nayib Elías <nelias@uninorte.edu.co>
2017-09-19 14:10:27 -0500

    agregando opcion de cambiar idioma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+
+	constructor(private translate: TranslateService){
+
+		translate.addLangs(['es', 'en']);
+		translate.setDefaultLang('es');
+		translate.use('es');
+	}
+
+	changeLang(lang: string){
+		this.translate.use(lang);
+	}
+
+
+  title = 'Curriculum';
 }
